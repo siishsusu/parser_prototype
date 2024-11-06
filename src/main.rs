a2990840231cacf887b55796a19fc37442f34304
siishsusu <byvladyslava@gmail.com>
2024-11-06 18:13:35 +0200

    reformated to unit tests

--- src/main.rs
+++ src/main.rs
@@ -11,57 +11,56 @@ fn main() -> anyhow::Result< () > {
     Ok(())
 }
 
-#[test]
-fn basic_test() -> anyhow::Result< () > {
-    let got = Grammar::parse(Rule::file, "-273.15,-15\n")?;
-    println!("{:?}", got);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn field_test() -> anyhow::Result< () > {
 
-    Ok(())
-}
+        let pair = Grammar::parse(Rule::field, "-273.15")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+        assert_eq!( pair.as_str(), "-273.15" );
+        assert_eq!( pair.as_span().start(), 0 );
+        assert_eq!( pair.as_span().end(), 7 );
 
-#[test]
-fn field_test() -> anyhow::Result< () > {
 
-    let pair = Grammar::parse(Rule::field, "-273.15")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
-    assert_eq!( pair.as_str(), "-273.15" );
-    assert_eq!( pair.as_span().start(), 0 );
-    assert_eq!( pair.as_span().end(), 7 );
+        let pair = Grammar::parse(Rule::field, "x");
+        assert!(pair.is_err());
 
-    let pair = Grammar::parse(Rule::field, "x");
-    assert!(pair.is_err());
+        let pair = Grammar::parse(Rule::field, "");
+        assert!(pair.is_err());
 
-    let pair = Grammar::parse(Rule::field, "");
-    assert!(pair.is_err());
+        Ok(())
+    }
 
-    Ok(())
-}
 
-#[test]
-fn record_test() -> anyhow::Result< () > {
+    #[test]
+    fn record_test() -> anyhow::Result< () > {
 
-    let pair = Grammar::parse(Rule::record, "-273.15,99")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
-    assert_eq!( pair.as_str(), "-273.15,99" );
-    assert_eq!( pair.as_span().start(), 0 );
-    assert_eq!( pair.as_span().end(), 10 );
+        let pair = Grammar::parse(Rule::record, "-273.15,99")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+        assert_eq!( pair.as_str(), "-273.15,99" );
+        assert_eq!( pair.as_span().start(), 0 );
+        assert_eq!( pair.as_span().end(), 10 );
 
-    let pair = Grammar::parse(Rule::record, "");
-    assert!( pair.is_err() );
+        let pair = Grammar::parse(Rule::record, "");
+        assert!( pair.is_err() );
 
-    Ok( () )
-}
+        Ok( () )
+    }
 
-#[test]
-fn file_test() -> anyhow::Result< () > {
-    let pair = Grammar::parse(Rule::file, "-273.15,99\n15,-5\n")?
-        .next()
-        .ok_or_else( || anyhow!( "no pair" ) )?;
+    #[test]
+    fn file_test() -> anyhow::Result< () > {
+        let pair = Grammar::parse(Rule::file, "-273.15,99\n15,-5\n")?
+            .next()
+            .ok_or_else( || anyhow!( "no pair" ) )?;
 
-    assert_eq!( pair.as_str(), "-273.15,99\n15,-5\n" );
-    assert_eq!( pair.as_span().start(), 0 );
-    assert_eq!( pair.as_span().end(), 17 );
+        assert_eq!( pair.as_str(), "-273.15,99\n15,-5\n" );
+        assert_eq!( pair.as_span().start(), 0 );
+        assert_eq!( pair.as_span().end(), 17 );
 
-    let pair = Grammar::parse(Rule::file, "-273.15,99,someText\n");
-    assert!(pair.is_err());
+        let pair = Grammar::parse(Rule::file, "-273.15,99,someText\n");
+        assert!(pair.is_err());
 
-    Ok(())
+        Ok(())
+    }
+
 }
